6052dbc6e4d24e03f60aae8298994872bb1e6797
Cédric Verger <webdcedric@gmail.com>
2013-04-14 00:51:58 +0200

    V1.02

--- web/index.php
+++ web/index.php
@@ -69,15 +69,4 @@ $app->get('/apiStats/texts/{ip}/{port}/{padId}/{secretKey}/stats/mostRepeated', 
     return $api->getRepeatedWord($api->getAllTextByPad());
 });
 
-
-$app->get('/apiStats/language/fr', function() {
-    return 'Charles Hermite2 naît le 24 décembre 1822 à Dieuze, en Lorraine. Il est le sixième d une famille de sept enfants. Son père, Ferdinand Hermite, d abord ingénieur dans une entreprise de salines, s’était tourné vers le négoce du drap après son mariage avec Madeleine Lallemand, fille d un marchand dans cette branche. En 1828, la famille Hermite déménage à Nancy. Le jeune Charles Hermite souffre d une malformation au pied droit, qui le handicape dans ses déplacements.';
-});
-
-$app->get('/apiStats/language/en', function()
-{
-    return 'Born in Dieuze, Moselle on 24 December 1822, he was the son of a salt mine engineer, Ferdinand Hermite. His mother was Madeleine Lallemand. The family moved to run a drapers business in Nancy in 1828 and his father also pursued ambitions as an artist. Charles was the sixth of his parents';
-});
-
-
 $app->run();
